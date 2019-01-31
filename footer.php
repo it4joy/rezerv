f34acce80ac08b77495408c2f228a225135cc4e1
Igor Ivanov <drkierkegor@gmail.com>
2019-01-31 13:53:55 +0300

    sm updt

--- footer.php
+++ footer.php
@@ -4,10 +4,9 @@
 	<footer id="site-footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-3">&copy; ТК «Резерв», 2011-2015 <br> Все права защищены</div>
-				<div class="col-md-3">Разработка сайта: <a href="http://sensitivegraphics.com/" target="_blank">Sensitive Graphics</a></div>
-				<div class="col-md-3">Яндекс.Метрика</div>
-				<div class="col-md-3"> <div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,moimir,gplus,twitter,linkedin,lj,viber,whatsapp" data-size="s"></div> </div>
+				<div class="col-md-4">&copy; ТК «Резерв», 2011-2019 <br> Все права защищены</div>
+				<div class="col-md-4">Разработка сайта: <a href="https://graphicalshell.ru/" target="_blank">Graphical Shell</a></div>
+				<div class="col-md-4"> <div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,moimir,gplus,twitter,linkedin,lj,viber,whatsapp" data-size="s"></div> </div>
 			</div>
 		</div>
 	</footer>
